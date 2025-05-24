a8aabaa0f92c7291a8e15e3ba4e51f4ce09669c8
moumnassi <moumnassi245@gmail.com>
2025-05-24 19:20:43 +0200

    add pycaret test and fix plots

--- src/data_modeling/data_modeling.py
+++ src/data_modeling/data_modeling.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')  # Use a non-interactive backend for matplotlib
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
@@ -42,12 +44,12 @@ class Modeling:
         if model_selection == 'classification':
             classification_setup = setup(data, target=target, session_id=123)
             best_model = compare_models()
-            save_model(best_model, '/result/classification')
+            save_model(best_model, 'result/classification')
 
         elif model_selection == 'regression':
             regression_setup = setup(data, target=target, session_id=123)
             best_model = compare_models()
-            save_model(best_model, '/result/regression')
+            save_model(best_model, 'result/regression')
         else:
             raise ValueError("model_selection must be either 'classification' or 'regression'")
         return best_model
@@ -61,7 +63,7 @@ class Modeling:
         """
         eval_ = evaluate_model(model)
         # save the evaluation results
-        plt.savefig('/result/evaluation.png')
+        plt.savefig('result/evaluation.png')
 
         return eval_
     
@@ -72,19 +74,19 @@ class Modeling:
     )-> None:
         plot_model(model, plot='auc')
         #save the plot
-        plt.savefig('/result/auc.png')
+        plt.savefig('result/auc.png')
         plot_model(model, plot='feature')
         #save the plot
-        plt.savefig('/result/feature.png')
+        plt.savefig('result/feature.png')
         plot_model(model, plot='confusion_matrix')
         #save the plot
-        plt.savefig('/result/confusion_matrix.png')
+        plt.savefig('result/confusion_matrix.png')
         plot_model(model, plot='pr')
         #save the plot
-        plt.savefig('/result/pr.png')
+        plt.savefig('result/pr.png')
         plot_model(model, plot='learning')
         #save the plot
-        plt.savefig('/result/learning.png')
+        plt.savefig('result/learning.png')
         
     
 
@@ -96,14 +98,14 @@ class Modeling:
     ):
         prediction = predict_model(model, data=test_data)
         #save the prediction
-        prediction.to_csv('/result/prediction.csv', index=False)
+        prediction.to_csv('result/prediction.csv', index=False)
         return prediction
     
     def pycaret_save_model(
             self,
             model,
     ):
-        save_model(model, '/result/model')
+        save_model(model, 'result/model')
         return model
     def pycaret_load_model(
             self,
@@ -118,12 +120,12 @@ class Modeling:
         if model_selection == 'classification':
             best_model = compare_models()
             # save the best model
-            save_model(best_model, '/result/classification')
+            save_model(best_model, 'result/classification')
             return best_model
         elif model_selection == 'regression':
             best_model = compare_models()
             # save the best model
-            save_model(best_model, '/result/regression')
+            save_model(best_model, 'result/regression')
             return best_model
         else:
             raise ValueError("model_selection must be either 'classification' or 'regression'")
